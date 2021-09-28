de87cd18c3e4e3e079d316eca6850e3327abdeea
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-09-28 22:21:48 +0900

    🔥 react-native-screens로 네이티브 스크린 사용하기(안드로이드)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {ThemeProvider} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import {Appearance, AppRegistry} from 'react-native';
+import {enableScreens} from 'react-native-screens';
 import {Provider} from 'react-redux';
 import {AppNavigator} from './src/navigations/AppNavigation';
 import configureStore from './src/redux/store/index';
@@ -10,6 +11,8 @@ const store = configureStore();
 
 const App = () => {
   const [colorScheme, setColorScheme] = useState(Appearance.getColorScheme());
+  enableScreens();
+
   useEffect(() => {
     Appearance.addChangeListener(({colorScheme}) => {
       setColorScheme(colorScheme);
