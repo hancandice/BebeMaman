c5c8e0eba2dd751cf35699d5c10bd108858ae189
Jeeyoung Han <64334381+hancandice@users.noreply.github.com>
2021-10-19 15:18:44 +0900

    🔥 assets 추가

--- src/Core/onboarding/WalkthroughScreen/WalkthroughScreen.js
+++ src/Core/onboarding/WalkthroughScreen/WalkthroughScreen.js
@@ -2,7 +2,12 @@ import React from 'react';
 import {Text} from 'react-native';
 
 const WalkthroughScreen = props => {
-  console.log(props);
+  const {navigation} = props;
+
+  const appConfig =
+    navigation.state.params.appConfig || navigation.getParam('appConfig');
+  const appStyles =
+    navigation.state.params.appStyles || navigation.getParam('appStyles');
 
   return <Text>WalkthroughScreen</Text>;
 };
